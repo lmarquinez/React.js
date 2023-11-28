30ee50b2639ccf125f3f526afec7b78440ea37d9
Lauri <rapiri4@hotmail.com>
2023-11-28 01:12:01 +0100

    Search products in the api

--- actividad2/src/pages/Shop.jsx
+++ actividad2/src/pages/Shop.jsx
@@ -1,13 +1,78 @@
-import logo from "../../../actividad1/src/assets/image/logo.svg";
+import { useState, useEffect } from "react";
+import { getProducts } from "../app/api";
+import styled from "styled-components";
 import "../App.css";
 
+const ShopWrapper = styled.div`
+  text-align: center;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  padding: 8px;
+  margin-right: 8px;
+`;
+
+const Button = styled.button`
+  padding: 8px 16px;
+  background-color: #3498db;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+`;
+
+const ProductList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin-top: 20px;
+  overflow: scroll;
+  height: 58vh;
+`;
+
+const ProductItem = styled.div`
+  width: 200px;
+  margin: 10px;
+  text-align: center;
+`;
+
+const ProductImage = styled.img`
+  max-width: 100%;
+  height: auto;
+`;
+
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+  const [searchProduct, setSearchProduct] = useState("");
+
+  const handleSearch = async () => {
+    try {
+      setProducts(await getProducts(searchProduct));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
-    <div className="shop-main">
-      <h2>
-        <code>Shop</code> component
-      </h2>
-    </div>
+    <ShopWrapper>
+      <h2>Shop component</h2>
+      <Input
+        type="text"
+        placeholder="Enter product term..."
+        value={searchProduct}
+        onChange={(e) => setSearchProduct(e.target.value)}
+      />
+      <Button onClick={handleSearch}>Search</Button>
+      <ProductList>
+        {products.map((product) => (
+          <ProductItem key={product.id}>
+            <ProductImage src={product.thumbnail} alt={product.title} />
+            <p>{product.title}</p>
+            <p>${product.price}</p>
+          </ProductItem>
+        ))}
+      </ProductList>
+    </ShopWrapper>
   );
 };
 
