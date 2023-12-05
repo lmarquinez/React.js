34912458f9010e140c33dcc16d73de8f592511f7
Lauri <rapiri4@hotmail.com>
2023-12-05 20:03:11 +0100

    add pwa

--- actividad2-pwa/vite.config.js
+++ actividad2-pwa/vite.config.js
@@ -2,7 +2,41 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "actividad2-pwa",
+    short_name: "actividad2-pwa",
+    description: "I am a simple vite app to practice with the routing and APIs",
+    icons: [
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#094d7b",
+    background_color: "#094d7b",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
 });
