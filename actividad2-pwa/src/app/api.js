6eb505d6f50ee78ddbf7b979c06a00f02935d15e
Lauri <rapiri4@hotmail.com>
2024-01-09 17:29:25 +0100

    removed activity

--- actividad2-pwa/src/app/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const getProducts = async (searchProduct) => {
-  const res = await axios.get(
-    `https://api.mercadolibre.com/sites/MLA/search?q=${searchProduct}`
-  );
-  return res.data.results;
-};
