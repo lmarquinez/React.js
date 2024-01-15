979a73e88d2bdac2cd32863d0cf305322e79bf53
Lauri <rapiri4@hotmail.com>
2024-01-15 18:11:03 +0100

    update

--- rickymortyapi/src/app/api.jsx
+++ rickymortyapi/src/app/api.jsx
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const getCharacters = async () => {
   const res = await axios.get("https://rickandmortyapi.com/api/character");
+  //res.data.results.slice(0,5);
   return res.data.results;
 };
